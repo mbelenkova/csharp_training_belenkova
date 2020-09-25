7ebc453d2e66e2a4e7908a4fca84162901189b60
Maria Belenkova <maschynja2009@yandex.ru>
2020-09-25 10:22:49 +0300

    Добавьте файлы проекта.

---
+++ addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a050bf4-1862-47d3-b641-b79795fa2015")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+       
+        private AutoItX3 aux;
+        public static string WINTITLE = "Free Address Book";
+        private GroupHelper groupHelper;
+        
+        public ApplicationManager()
+        {
+            
+            aux = new AutoItX3();
+            aux.Run(@"C:\Users\Home\Desktop\FreeAddressBookPortable\AddressBook.exe","",aux.SW_SHOW);//первый параметр путь к запускаемому приложению,остальные параметры можно проигнорировать
+            aux.WinWait(WINTITLE);
+            aux.WinActivate(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+
+
+            groupHelper = new GroupHelper(this);
+
+        }
+
+      
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE,"", "WindowsForms10.BUTTON.app.0.2c908d510"); // 3 параметра: 1 название окна в котором нужно нажимать кнопку,2 параметр - текст самой кнопки,3 параметр- идентификатор кнопки
+        }
+
+        public AutoItX3 Aux
+        {
+            get
+            {
+                return aux;
+            }
+        }
+
+        public GroupHelper Groups
+        {
+            get 
+            {
+                return groupHelper;
+            }
+        }
+
+    }
+}
